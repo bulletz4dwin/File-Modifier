f164646b161e5fb01ac52343eaca19cd09578f6b
Rishi <122477372+rizzshiii@users.noreply.github.com>
2023-08-14 18:02:47 +0530

    FileModifier.py

--- FileModifier.py
+++ FileModifier.py
@@ -14,7 +14,7 @@ def rename_files():
 
     message_label.config(text="Made By rizzshiii on GitHub.")
     root.update()
-    time.sleep(3)  # Display the message for 5 seconds
+    time.sleep(3) 
     message_label.config(text="Renaming files...")
 
     files = os.listdir(folder_path)
@@ -24,10 +24,10 @@ def rename_files():
         if os.path.isfile(os.path.join(folder_path, file_name)):
             old_name, old_extension = os.path.splitext(file_name)
 
-            # Use provided new filename or keep the old filename if blank
+         
             new_name = new_prefix + str(i) if not new_prefix == "" else old_name
 
-            # Use provided new extension if provided, otherwise keep the old extension
+           
             new_extension_with_dot = new_extension if new_extension else old_extension
 
             new_name_with_extension = f"{new_name}{new_extension_with_dot}"
@@ -40,19 +40,19 @@ def rename_files():
 root = tk.Tk()
 root.title("File Renamer by rizzshiii")
 
-# Set the dimensions of the window (width x height)
-window_width = 600  # Increased width
-window_height = 300  # Increased height
+
+window_width = 600  
+window_height = 300  
 root.geometry(f"{window_width}x{window_height}")
 
-# Configure the root window's background color
-root.configure(background="white")  # Set the background color to white
 
-# Create a themed style
+root.configure(background="white")
+
+
 style = ttk.Style()
-style.theme_use("clam")  # Use the "clam" theme for ttk elements
+style.theme_use("clam")  
 
-# Main frame to hold widgets
+
 main_frame = ttk.Frame(root)
 main_frame.pack(padx=20, pady=20, expand=True, fill="both")
 
@@ -75,9 +75,9 @@ new_extension_entry = tk.Entry(main_frame)
 new_extension_entry.grid(row=2, column=1, sticky="we")
 
 start_button = tk.Button(main_frame, text="Start", command=rename_files)
-start_button.grid(row=3, column=0, columnspan=2, sticky="we", pady=(10, 0))  # Added padding
+start_button.grid(row=3, column=0, columnspan=2, sticky="we", pady=(10, 0)) 
 
 message_label = tk.Label(root, text="")
-message_label.pack(side="bottom", anchor="w", padx=20, pady=(0, 20))  # Added padding
+message_label.pack(side="bottom", anchor="w", padx=20, pady=(0, 20))
 
 root.mainloop()
